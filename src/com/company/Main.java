689967bf3e27f36528e438a438e410181e0abf53
crisanjan <crisan.janclau97@gmail.com>
2022-06-16 14:02:07 +0300

    commited files for test

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+        public static String reverse(String str) {
+            return new StringBuilder(str).reverse().toString();
+        }
+
+        public static void main(String[] args)
+        {
+            String str = "The Internet";
+
+            // Note that string is immutable in Java
+            str = reverse(str);
+
+            System.out.println("The reverse of the given string is: " + str);
+        }
+    }
+
+
